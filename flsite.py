da60948bb118d032ee4259f402ff2b21f02b63a3
GabdoDoni <dannygabdullin@gmail.com>
2023-05-20 15:19:34 +0600

    create static files images and update html

--- flsite.py
+++ flsite.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+from FDataBase import FDataBase
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -11,9 +12,9 @@ app.config.from_object(__name__)
 
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
-menu = [{'name': 'Установка', 'url': 'install-flask'},
-        {'name': 'Первое приложение', 'url': 'first-app'},
-        {'name': 'Обратная связь', 'url': 'contact'}]
+# menu = [{'name': 'Установка', 'url': 'install-flask'},
+#         {'name': 'Первое приложение', 'url': 'first-app'},
+#         {'name': 'Обратная связь', 'url': 'contact'}]
 
 
 def connect_db():
@@ -40,13 +41,46 @@ def get_db():
 @app.route('/')
 def index():
     db = get_db()
-    return render_template('index.html', menu=menu)
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+
+@app.route('/add_post', methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка при добавлении статьи')
+            else:
+                flash('Статья добавлено')
+        else:
+            flash('Ошибка при добавлении статьи')
+
+    return render_template('add_post.html', title='Создание статьи', menu=dbase.getMenu())
+
+
+@app.route('/post/<alias>')
+def showPost(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+
+    title, text = dbase.getPost(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=text)
+
+
 #
 #
 # @app.route('/about')
