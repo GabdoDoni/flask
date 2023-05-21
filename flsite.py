547a1f225f119e6596d2ecef9a5f6e3fa8fb6b72
GabdoDoni <dannygabdullin@gmail.com>
2023-05-21 20:52:54 +0600

    login registration logout

--- flsite.py
+++ flsite.py
@@ -2,6 +2,9 @@ import sqlite3
 import os
 from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
 from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from UserLogin import UserLogin
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -12,6 +15,17 @@ app.config.from_object(__name__)
 
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Авторизуйтесь для доступа страницы'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print('load user')
+    return UserLogin().fromDB(user_id, dbase)
+
+
 # menu = [{'name': 'Установка', 'url': 'install-flask'},
 #         {'name': 'Первое приложение', 'url': 'first-app'},
 #         {'name': 'Обратная связь', 'url': 'contact'}]
@@ -21,6 +35,16 @@ def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
+
+
+dbase = None
+
+
+@app.before_request
+def before_request():
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
 
 
 def create_db():
@@ -40,8 +64,6 @@ def get_db():
 @app.route('/index')
 @app.route('/')
 def index():
-    db = get_db()
-    dbase = FDataBase(db)
     return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
@@ -53,9 +75,6 @@ def close_db(error):
 
 @app.route('/add_post', methods=['POST', 'GET'])
 def addPost():
-    db = get_db()
-    dbase = FDataBase(db)
-
     if request.method == 'POST':
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
             res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
@@ -70,10 +89,8 @@ def addPost():
 
 
 @app.route('/post/<alias>')
+@login_required
 def showPost(alias):
-    db = get_db()
-    dbase = FDataBase(db)
-
     title, text = dbase.getPost(alias)
     if not title:
         abort(404)
@@ -99,23 +116,65 @@ def showPost(alias):
 #     return render_template('contact.html', title='Обратная связь', menu=menu)
 #
 #
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if 'userLogged' in session:
-#         return redirect(url_for('profile', username=session['userLogged']))
-#     elif request.method == 'POST' and request.form['username'] == 'doni' and request.form['password'] == '124':
-#         session['userLogged'] = request.form['username']
-#         return redirect(url_for('profile', username=session['userLogged']))
-#
-#     return render_template('login.html', title='Авторизация', menu=menu)
-#
-#
-# @app.route('/profile/<username>')
-# def profile(username):
-#     if 'userLogged' not in session or session['userLogged'] != username:
-#         abort(401)
-#
-#     return f'Профиль пользователя {username}'
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # if 'userLogged' in session:
+    #     return redirect(url_for('profile', username=session['userLogged']))
+    # elif request.method == 'POST' and request.form['username'] == 'doni' and request.form['password'] == '124':
+    #     session['userLogged'] = request.form['username']
+    #     return redirect(url_for('profile', username=session['userLogged']))
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
+    if request.method == 'POST':
+        user = dbase.getUserByEmail(request.form['email'])
+        if user and check_password_hash(user['password'], request.form['password']):
+            userlogin = UserLogin().create(user)
+            rm = True if request.form.get('remainme') else False
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get('next') or url_for('profile'))
+
+        flash('Неверно заполнены поля', 'error')
+
+    return render_template('login.html', title='Авторизация', menu=dbase.getMenu())
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        if len(request.form['username']) > 4 and len(request.form['email']) > 4  \
+                 and len(request.form['password']) > 4 and request.form['password'] == request.form['password2']:
+            hash = generate_password_hash(request.form['password'])
+            res = dbase.addUser(request.form['username'], request.form['email'], hash)
+
+            if res:
+                flash('Новый пользователь успешно создан', 'success')
+                return redirect(url_for('login'))
+            else:
+                flash('Ошибка при добавлении в БД', 'error')
+        else:
+            flash('Неверно заполнены поля', 'error')
+
+    return render_template('register.html', title='Регистрация', menu=dbase.getMenu())
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из аккаунта', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    # if 'userLogged' not in session or session['userLogged'] != username:
+    #     abort(401)
+    #
+    # return f'Профиль пользователя {username}'
+    return f'''<p><a href="{url_for('logout')}">Выйти из профиля</a>
+                <p>user info: {current_user.get_id()}'''
 #
 #
 # @app.errorhandler(404)
--- sq_db.sql
+++ sq_db.sql
@@ -11,3 +11,11 @@ text text NOT NULL,
 url text NOT NULL,
 time integer NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS users (
+id integer PRIMARY KEY AUTOINCREMENT,
+username text NOT NULL,
+email text NOT NULL,
+password text NOT NULL,
+time integer NOT NULL
+);
