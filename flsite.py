5e96d1661f0c5ef246b6054b04379012f6c67757
GabdoDoni <dannygabdullin@gmail.com>
2023-05-23 00:07:49 +0600

    admin panel and sqlalchemy

--- flsite.py
+++ flsite.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
 from forms import *
+from admin.admin import admin
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -16,6 +17,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+app.register_blueprint(admin, url_prefix='/admin')
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
